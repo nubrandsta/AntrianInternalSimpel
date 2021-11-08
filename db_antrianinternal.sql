fdb30b62f5b680d7d76868bfca210fab4b57f6f9
nubrandsta <50097389+nubrandsta@users.noreply.github.com>
2021-11-08 13:16:38 +0700

    Updated Database

--- db_antrianinternal.sql
+++ db_antrianinternal.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 07, 2021 at 03:41 PM
--- Server version: 10.4.20-MariaDB
--- PHP Version: 7.3.29
+-- Generation Time: Nov 08, 2021 at 07:16 AM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -39,11 +39,10 @@ CREATE TABLE `tb_antrian` (
 --
 
 INSERT INTO `tb_antrian` (`id_pesanan`, `id_menu`, `jumlah`, `no_urut`) VALUES
-('AB0711210103', 'AB01', 2, 3),
-('AG0711210105', 'AG01', 2, 5),
-('AG0711210204', 'AG02', 1, 4),
-('MA0711210101', 'MA01', 1, 1),
-('MA0711210202', 'MA02', 1, 2);
+('AB0711210103', 'AB01', 2, 2),
+('AB0811210136', 'AB01', 3, 4),
+('AG0711210204', 'AG02', 1, 3),
+('MA0711210202', 'MA02', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -70,6 +69,30 @@ INSERT INTO `tb_menu` (`id_menu`, `nama_menu`, `harga_menu`, `kategori_menu`) VA
 ('MA01', 'Mie Ayam', 10000, 'Mie Ayam'),
 ('MA02', 'Bakso', 10000, 'Mie Ayam');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_transaksi`
+--
+
+CREATE TABLE `tb_transaksi` (
+  `id_transaksi` varchar(12) NOT NULL,
+  `kategori_menu` varchar(25) NOT NULL,
+  `id_menu` varchar(4) NOT NULL,
+  `nama_menu` varchar(25) NOT NULL,
+  `harga_menu` int(11) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `total` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_transaksi`
+--
+
+INSERT INTO `tb_transaksi` (`id_transaksi`, `kategori_menu`, `id_menu`, `nama_menu`, `harga_menu`, `jumlah`, `total`) VALUES
+('MA0711210101', 'Mie Ayam', 'MA01', 'Mie Ayam', 10000, 1, 10000),
+('MA081121026', 'Mie Ayam', 'MA02', 'Bakso', 10000, 5, 50000);
+
 --
 -- Indexes for dumped tables
 --
@@ -85,6 +108,12 @@ ALTER TABLE `tb_antrian`
 --
 ALTER TABLE `tb_menu`
   ADD PRIMARY KEY (`id_menu`);
+
+--
+-- Indexes for table `tb_transaksi`
+--
+ALTER TABLE `tb_transaksi`
+  ADD PRIMARY KEY (`id_transaksi`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
